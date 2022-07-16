5e1cb763db45f191b3ae875d841334f5919141c5
jpagan2 <jpagan2@hawk.iit.edu>
2022-07-16 15:36:48 -0400

    created all necessary modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountsModule } from './accounts/accounts.module';
+import { AsksModule } from './asks/asks.module';
+import { GivesModule } from './gives/gives.module';
+import { ThanksModule } from './thanks/thanks.module';
+import { NotesModule } from './notes/notes.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [AccountsModule],
+  imports: [AccountsModule, AsksModule, GivesModule, ThanksModule, NotesModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
