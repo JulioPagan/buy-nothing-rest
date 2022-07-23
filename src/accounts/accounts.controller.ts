75c6ce764043abbf2178015df77ad9da04ed113e
jpagan2 <jpagan2@hawk.iit.edu>
2022-07-23 17:44:37 -0400

    find account by 'id' Param

--- src/accounts/accounts.controller.ts
+++ src/accounts/accounts.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { Account } from 'src/interfaces/account.interface';
 import { CreateAccountDto } from 'src/dto/create-account.dto';
@@ -8,12 +8,16 @@ export class AccountsController {
     constructor(private accountsService: AccountsService){}
 
     @Post()
-    async create(@Body() createAccountDto: CreateAccountDto){
+    async create(@Body() createAccountDto: CreateAccountDto) {
         this.accountsService.create(createAccountDto);
     }
     
     @Get()
     async findAccounts(): Promise<Account[]> {
         return this.accountsService.findAll();
+    }
+    @Get(':id')
+    public findOneAccount(@Param('id', ParseIntPipe) id: number): Account {
+        return this.accountsService.findOne(id);
     }
 }
--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -1,14 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, Param, ParseIntPipe } from '@nestjs/common';
 import { Account } from '../interfaces/account.interface';
 
 @Injectable()
 export class AccountsService {
     private readonly accounts: Account[] = [];
 
-    create(account: Account) {
+    create(account: Account): Account {
+        
         this.accounts.push(account);
+
+        return account;
     }
     findAll(): Account[] {
         return this.accounts
+    }
+    public findOne(id: number): Account {
+        const account: Account = this.accounts.find(account => account.id === id);
+
+        if (!account) {
+            throw new NotFoundException('Account Not Found');
+        }
+        return account;
     }
 }
