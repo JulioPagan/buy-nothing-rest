c78e788b7bc71bb846a2d9d2532b68e8b595f574
jpagan2 <jpagan2@hawk.iit.edu>
2022-09-14 20:54:42 -0400

    updated testing

--- src/notes/notes.service.spec.ts
+++ src/notes/notes.service.spec.ts
@@ -1,3 +1,4 @@
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotesService } from './notes.service';
 
@@ -22,6 +23,34 @@ describe('NotesService', () => {
     description: 'this is another test note',
     date_created: null
   }
+  let testThread = {
+    uid: 1,
+    nid: null,
+    to_type: 'ask',
+    to_user_id: 0,
+    to_id: 0,
+    description: 'this is a test thread',
+    date_created: null
+  }
+  let testThread1 = {
+    uid: 1,
+    nid: null,
+    to_type: 'note',
+    to_user_id: 0,
+    to_id: 0,
+    description: 'this is a test thread',
+    date_created: null
+  }
+  let testThread2 = {
+    uid: 1,
+    nid: null,
+    to_type: 'note',
+    to_user_id: 1,
+    to_id: 0,
+    description: 'this is a test thread',
+    date_created: null
+}
+
   let updatedNote = {
     uid: 1,
     nid: 0,
@@ -31,6 +60,7 @@ describe('NotesService', () => {
     description: 'this is another test note',
     date_created: null
   }
+
 
 
   beforeEach(async () => {
@@ -44,6 +74,8 @@ describe('NotesService', () => {
   });
 
 
+
+  // Test create note
   it('should create a note with NID', () => {
     let createdNote = service.create(testNote1);
     let nid = createdNote.nid;
@@ -59,8 +91,142 @@ describe('NotesService', () => {
         date_created: date
       });
   });
+  // Test create note
+  it('should create a new thread with NID', () => {
+    let createdNote = service.create(testThread);
+    let nid = createdNote.nid;
+    let date = createdNote.date_created;
+    expect(createdNote).toEqual(
+      {
+        uid: 1,
+        nid: nid,
+        to_type: 'ask',
+        to_user_id: 0,
+        to_id: 0,
+        description: 'this is a test thread',
+        date_created: date
+    
+      });
+  });
+  // Test create note
+  it('should create a response', () => {
+    let createdNote = service.create(testThread1);
+    let nid = createdNote.nid;
+    let date = createdNote.date_created;
+    expect(createdNote).toEqual(
+      {
+        uid: 1,
+        nid: nid,
+        to_type: 'note',
+        to_user_id: 0,
+        to_id: 0,
+        description: 'this is a test thread',
+        date_created: date
+      });
+  });
+  // Test create note
+  it('should create a response', () => {
+    let createdNote = service.create(testThread2);
+    let nid = createdNote.nid;
+    let date = createdNote.date_created;
+    expect(createdNote).toEqual(
+      {
+        uid: 1,
+        nid: nid,
+        to_type: 'note',
+        to_user_id: 1,
+        to_id: 0,
+        description: 'this is a test thread',
+        date_created: date
+      });
+  });
+  // Test BAD request when pre-setting NID
+  it('should throw BAD REQUEST if attempting too pre-set tid', () => {
+    expect(() => {service.create({
+      uid: 0,
+      nid: 9,
+      to_type: 'give',
+      to_user_id: 0,
+      to_id: 0,
+      description: 'this is a test note',
+      date_created: null
+    })}).toThrow(BadRequestException);
+  });
+  // Test BAD request when pre-setting date_created
+  it('should throw BAD REQUEST if attempting too pre-set date_created', () => {
+    expect(() => {service.create({
+      uid: 0,
+      nid: null,
+      to_type: 'give',
+      to_user_id: 0,
+      to_id: 0,
+      description: 'this is a test note',
+      date_created: '2022-03-12T17:12:26Z'
+    })}).toThrow(BadRequestException);
+  });
+  // Test BAD request when UID is missing
+  it('should throw BAD REQUEST if uid is missing', () => {
+    expect(() => {service.create({
+      uid: null,
+      nid: null,
+      to_type: 'give',
+      to_user_id: 0,
+      to_id: 0,
+      description: 'this is a test note',
+      date_created: null
+    })}).toThrow(BadRequestException);
+  });
+  // Test BAD request when to_type is missing
+  it('should throw BAD REQUEST if to_type is missing', () => {
+    expect(() => {service.create({
+      uid: 0,
+      nid: null,
+      to_type: null,
+      to_user_id: 0,
+      to_id: 0,
+      description: 'this is a test note',
+      date_created: null
+    })}).toThrow(BadRequestException);
+  });
+  // Test BAD request when to_user_id is missing
+  it('should throw BAD REQUEST if to_user_id is missing', () => {
+    expect(() => {service.create({
+      uid: 0,
+      nid: null,
+      to_type: 'give',
+      to_user_id: null,
+      to_id: 0,
+      description: 'this is a test note',
+      date_created: null
+    })}).toThrow(BadRequestException);
+  });
+  // Test BAD request when to_id is missing
+  it('should throw BAD REQUEST if to_id is missing', () => {
+    expect(() => {service.create({
+      uid: 0,
+      nid: null,
+      to_type: 'give',
+      to_user_id: 0,
+      to_id: null,
+      description: 'this is a test note',
+      date_created: null
+    })}).toThrow(BadRequestException);
+  });
+  // Test BAD request when description is missing
+  it('should throw BAD REQUEST description is missing', () => {
+    expect(() => {service.create({
+      uid: 0,
+      nid: null,
+      to_type: 'give',
+      to_user_id: 0,
+      to_id: 0,
+      description: null,
+      date_created: null
+    })}).toThrow(BadRequestException);
+  });
 
 
+  // Test update
   it('should update the pre-existng note with new note', () => {
     service.updateNote(service.notes[0].nid, updatedNote);
     let date = service.notes[0].date_created;
@@ -74,29 +240,46 @@ describe('NotesService', () => {
       date_created: date
     });
   });
+  // Test BAD update
+  it('should throw 404 if NID not found', () => {
+    expect(() => {service.updateNote(9, updatedNote)}).toThrow(NotFoundException);
+  });
 
+
+  // Test delete
   it('should delete the note identified by NID', () => {
     let preDelete = service.notes.length;
     service.deleteNote(service.notes[0].nid);
     expect(preDelete == service.notes.length).toBeFalsy();
   });
+  // Test BAD delete
+  it('should throw 404 if NID not found', () => {
+    expect(() => {service.deleteNote(9)}).toThrow(NotFoundException);
+  });
 
 
+  // Test viewNotes
   it('should find all notes in the existing list of notes when the user viewing them is CSR', () => {
-    // User #2 is the Customer Service Representative (CSR)
     let allNotesby0 = service.viewNotes(0, 2);
     expect(allNotesby0).toEqual(service.viewNotes(0, 2));
   });
+  it('should find all notes', () => {
+    let allNotes = service.viewNotes();
+    expect(service.viewNotes()).toEqual(allNotes);
+  });
 
-  // Test viewing notes withing specified dates
-
-  // Test viewing Notes as RU
 
 
+
+  // Test viewNote
   it('should find one note identified by the NID', () => {
     let index = 0;
     let firstNote = service.viewNote(index);
     expect(firstNote == service.notes[index]).toBeTruthy();
+  });
+  // Test BAD viewNote
+  it('should throw 404 if no note found', () => {
+    expect(() => {service.viewNote(9)}).toThrow(NotFoundException);
   });
 
 
--- src/thanks/thanks.service.spec.ts
+++ src/thanks/thanks.service.spec.ts
@@ -1,4 +1,4 @@
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ThanksService } from './thanks.service';
 
@@ -19,6 +19,14 @@ describe('ThanksService', () => {
     description: "This is a very grateful test thank",
     date_created: null,
   }
+  let testThankCreation = {
+    uid: 0,
+    tid: null,
+    thank_to: 0,
+    description: "This is a creation test thank",
+    date_created: null,
+  }
+
   let updatedThank = {
     uid: 0,
     tid: 0,
@@ -26,6 +34,8 @@ describe('ThanksService', () => {
     description: "This is an updated thank-you",
     date_created: null,
   }
+
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -37,8 +47,11 @@ describe('ThanksService', () => {
     service.createThank(testThank2);
   });
 
+
+
+  // Test create
   it('should create a thank with TID', () => {
-    let createdThank = service.createThank(testThank1);
+    let createdThank = service.createThank(testThankCreation);
     let tid = createdThank.tid;
     let date = createdThank.date_created;
     expect(createdThank).toEqual(
@@ -46,11 +59,11 @@ describe('ThanksService', () => {
         uid: 0,
         tid: tid,
         thank_to: 0,
-        description: "This is a test thank",
+        description: "This is a creation test thank",
         date_created: date,
       });
   });
-
+  // Test BAD request when pre-setting TID
   it('should throw BAD REQUEST if attempting too pre-set tid', () => {
       expect(() => {service.createThank({
         uid: 0,
@@ -60,8 +73,7 @@ describe('ThanksService', () => {
         date_created: null,
         })}).toThrow(BadRequestException);
     })
-
-  
+  // Test BAD request when pre-setting date_createddd
   it('should throw BAD REQUEST if attempting too pre-set date_created', () => {
     expect(() => {service.createThank({
       uid: 0,
@@ -71,55 +83,105 @@ describe('ThanksService', () => {
       date_created: '2022-03-12T17:12:26Z',
       })}).toThrow(BadRequestException);
   })
+  // Test BAD request when missing uid
+  it('should throw BAD REQUEST if attempting too pre-set date_created', () => {
+    expect(() => {service.createThank({
+      uid: null,
+      tid: null,
+      thank_to: 0,
+      description: "This is an attempt to create thank",
+      date_created: null,
+      })}).toThrow(BadRequestException);
+  })
+  // Test BAD request when missing thank_to
+  it('should throw BAD REQUEST if missing thank_to', () => {
+    expect(() => {service.createThank({
+      uid: 0,
+      tid: null,
+      thank_to: null,
+      description: "This is an attempt to create thank",
+      date_created: null,
+      })}).toThrow(BadRequestException);
+  })
+  // Test BAD request when missing description
+  it('should throw BAD REQUEST if missing description', () => {
+    expect(() => {service.createThank({
+      uid: 0,
+      tid: null,
+      thank_to: 0,
+      description: null,
+      date_created: null,
+      })}).toThrow(BadRequestException);
+  })
 
   
-  // Test is required fields are not satisfied
-
+  // Test update
   it('should update the pre-existng thank with new thank', () => {
     service.update(service.thanks[0].uid, service.thanks[0].tid, updatedThank);
     expect(service.thanks[0]).toEqual(updatedThank);
   });
-
-
-  it('should create an thank with tid', () => {
-    let createdThank = service.createThank(testThank2);
-    let tid = createdThank.tid;
-    let date = createdThank.date_created;
-    expect(createdThank).toEqual(
-      {
-        uid: 0,
-        tid: tid,
-        thank_to: 0,
-        description: "This is a very grateful test thank",
-        date_created: date,
-      });
+  // Test BAD update
+  it('should throw 404 if TID not found', () => {
+    expect(() => {service.update(0, 9, updatedThank)}).toThrow(NotFoundException);
+  });
+  // Test BAD update
+  it('should throw 404 if UID not found', () => {
+    expect(() => {service.update(9, 0, updatedThank)}).toThrow(NotFoundException);
   });
 
-
-  // TO-DO: Test finding my thanks
+  
+  // Test get my thanks
   it('should find all "my" active thanks', () => {
     let myThanks = service.thanks.filter(thank => { 
       return (thank.uid == 1);
   });;
     expect(service.getMyThanks(1)).toEqual(myThanks);
   });
-
-
-  it('should find all thanks in the existing list of thanks', () => {
-    // User #2 is the Customer Service Representative (CSR)
-    let allThanks = service.findAll();
-    expect(allThanks == service.thanks).toEqual(true);
+  // Test Bad get request
+  it('should throw 404 if uid not found', () => {
+    expect(() => {service.getMyThanks(null)}).toThrow(NotFoundException);
   });
 
 
+  // Test find all
+  it('should find all thanks in the existing list of thanks', () => {
+    // User #2 is the Customer Service Representative (CSR)
+    let allThanks = service.findAll();
+    expect(allThanks == service.thanks).toBeTruthy();
+  });
+
+  // Test findOne
   it('should find one thank identified by the TID', () => {
     // User #2 is the Customer Service Representative (CSR)
     let index = 0;
     let firstThank = service.findOne(index);
-    expect(firstThank == service.thanks[index]).toEqual(true);
+    expect(firstThank == service.thanks[index]).toBeTruthy();
+  });
+  // Test BAD findOne
+  it('should throw 404 if TID not found', () => {
+    expect(() => {service.findOne(null)}).toThrow(NotFoundException);
   });
 
 
+  // Test findAllForUser
+  it('should find thanks for user', () => {
+    console.log(service.findAllForUser(0));
+    let filteredSearch = service.thanks.filter(thank => { 
+      return (thank.thank_to == 0)});
+    expect(service.findAllForUser(0)).toEqual(filteredSearch);
+  });
+  // Test findAllForUser
+  it('should throw 404 if no thanks for user found', () => {
+    expect(() => {service.findAllForUser(null)}).toThrow(NotFoundException);
+  });
+
+
+  // Test Search
+  it('should find all thanks that match keyword', () => {
+    let searchResults = service.thanks
+    expect(service.searchThanks()).toEqual(searchResults);
+  });
+  // Test Search by keyword
   it('should find all thanks that match keyword', () => {
     let searchedThanks = service.searchThanks('is');
     let searchResults = service.thanks.filter(thank => { 
@@ -128,7 +190,6 @@ describe('ThanksService', () => {
       return thankDescription.includes('is'.toLowerCase())})
     expect(searchedThanks).toEqual(searchResults);
   });
-
   // Search thanks by keyword and start_date
   it('should find all thanks that match keyword and after start date', () => {
     let start = new Date('31-Dec-2000');
@@ -138,17 +199,15 @@ describe('ThanksService', () => {
       return (thankDescription.includes('created'.toLowerCase())) && ((thank.date_created > start))});
     expect(searchedThanks.join() == filteredSearch.join()).toBeTruthy();
   });
-
   // Search thanks by keyword and end_date
   it('should find all thanks that match keyword and before end date', () => {
     let end = new Date('31-Dec-2030');
-    let searchedThanks = service.searchThanks('created', end);
+    let searchedThanks = service.searchThanks('created', null, end);
     let filteredSearch = service.thanks.filter(thank => { 
       let thankDescription = thank.description.toLowerCase();
       return (thankDescription.includes('created'.toLowerCase()) ) && ((thank.date_created < end))});
     expect(searchedThanks.join() == filteredSearch.join()).toBeTruthy();
   });
-
   // Search thanks by keyword and in between date range
   it('should find all thanks that match keyword and between start and end date', () => {
     let start = new Date('31-Dec-2000');
